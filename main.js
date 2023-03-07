a8d310fa7541b19da247e21ea655430c912edfe4
DAlonsoGamerPro <109617599+DAlonsoGamerPro@users.noreply.github.com>
2023-03-06 19:59:14 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,94 @@
+prediction_1 = ""
+prediction_2 = ""
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_photo()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    })
+}
+
+console.log('ml5 version', ml5.version);
+
+classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/SXbAakvwg/model.json', modelLoaded);
+
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data_1 = "La primera preedicción es: "+ prediction_1;
+    speak_data_2 = "Y la segunda preedicción es: "+ prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1, speak_data_2);
+    synth.speak(utterThis);
+}
+
+function gotResult(error, results)
+{
+    if(error){
+        console.error(error);
+    }
+    else
+    {
+        console.log(results);
+
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+
+        speak();
+
+        if(results[0].label == "feliz")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128522;";
+        }
+
+        if(results[0].label == "triste")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128532;";
+        }
+        
+        if(results[0].label == "enojado")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128544;";
+        }
+
+        //Los if que tienen 0 son los de la primera preedicción, mientras que los que tieen 1 son los de la segunda preedicción.
+
+        if(results[1].label == "feliz")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128522;";
+        }
+
+        if(results[1].label == "triste")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128532;";
+        }
+        
+        if(results[1].label == "enojado")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128544;";
+        }
+    }
+}
